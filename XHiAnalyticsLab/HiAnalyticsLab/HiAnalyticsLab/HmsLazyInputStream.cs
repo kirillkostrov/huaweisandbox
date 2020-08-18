87924ea408dbd3585efddf00aa46db99a6cb6369
kirill.kostrov <kostrov.kirill@huawei.com>
2020-08-18 16:00:28 +0300

    Xamarin HiAnalytics lab initial commit

---
+++ XHiAnalyticsLab/HiAnalyticsLab/HiAnalyticsLab/HmsLazyInputStream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Com.Huawei.Agconnect.Config;
+
+namespace KKHiAnalyticsStudy
+{
+    class HmsLazyInputStream : LazyInputStream
+    {
+        public HmsLazyInputStream(Context context) : base(context)
+        {
+        }
+
+        public override Stream Get(Context context)
+        {
+            try
+            {
+                return context.Assets.Open("agconnect-services.json");
+            }
+            catch (Exception e)
+            {
+                Log.Error("Hms", $"Failed to get input stream" + e.Message);
+                return null;
+            }
+        }
+    }
+}
