228a3ca6bd2c17b7541907735b0f736d347cc6c0
kirill.kostrov <kostrov.kirill@huawei.com>
2020-08-20 10:04:12 +0300

    tried both 5.0 and 4.0.3 plugins in both labs

---
+++ PushKitLab/PushKitLab/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Com.Huawei.Agconnect.Config;
+using System.IO;
+
+namespace PushKitLab
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void AttachBaseContext(Context context)
+        {
+            base.AttachBaseContext(context);
+            AGConnectServicesConfig config = AGConnectServicesConfig.FromContext(context);
+
+            var stream = new HmsLazyInputStream(context);
+            var reader = new StreamReader(stream.Get(context));
+            var content = reader.ReadToEnd();
+
+            config.OverlayWith(new HmsLazyInputStream(context));
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
