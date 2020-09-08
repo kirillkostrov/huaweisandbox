be084bcbb19b187005ba3d91b48b061ee0b8daa6
kirill.kostrov <kostrov.kirill@huawei.com>
2020-09-08 16:55:58 +0300

    splash ads java version done

--- Kits/Ads Kit/Java/app/src/main/java/com/huawei/hms/ads6/SplashActivity.java
+++ Kits/Ads Kit/Java/app/src/main/java/com/huawei/hms/ads6/SplashActivity.java
@@ -1,4 +1,180 @@
 package com.huawei.hms.ads6;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.AudioFocusType;
+import com.huawei.hms.ads.HwAds;
+import com.huawei.hms.ads.R;
+import com.huawei.hms.ads.splash.SplashAdDisplayListener;
+import com.huawei.hms.ads.splash.SplashView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    // Время показа рекламного блока в миллисекундах
+    private static final int AD_TIMEOUT = 5000;
+
+    private static final int MSG_AD_TIMEOUT = 1001;
+
+    private boolean hasPaused = false;
+
+    private SplashView splashView;
+
+    private Handler timeoutHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            if (SplashActivity.this.hasWindowFocus()) {
+                proceedToMainActivity();
+            }
+            return false;
+        }
+    });
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        HwAds.init(this);
+        loadAd();
+    }
+
+    private SplashView.SplashAdLoadListener splashAdLoadListener = new SplashView.SplashAdLoadListener() {
+        @Override
+        public void onAdLoaded() {
+            // Call this method when an ad is successfully loaded.
+            Log.i(TAG, "SplashAdLoadListener onAdLoaded.");
+            Toast.makeText(SplashActivity.this, getString(R.string.status_load_ad_success), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAdFailedToLoad(int errorCode) {
+            // Call this method when an ad fails to be loaded.
+            Log.i(TAG, "SplashAdLoadListener onAdFailedToLoad, errorCode: " + errorCode);
+            Toast.makeText(SplashActivity.this, getString(R.string.status_load_ad_fail) + errorCode, Toast.LENGTH_SHORT).show();
+            proceedToMainActivity();
+        }
+
+        @Override
+        public void onAdDismissed() {
+            // Call this method when the ad display is complete.
+            Log.i(TAG, "SplashAdLoadListener onAdDismissed.");
+            Toast.makeText(SplashActivity.this, getString(R.string.status_ad_dismissed), Toast.LENGTH_SHORT).show();
+            proceedToMainActivity();
+        }
+    };
+
+    private SplashAdDisplayListener adDisplayListener = new SplashAdDisplayListener() {
+        @Override
+        public void onAdShowed() {
+            // Call this method when an ad is displayed.
+            Log.i(TAG, "SplashAdDisplayListener onAdShowed.");
+        }
+
+        @Override
+        public void onAdClick() {
+            // Call this method when an ad is clicked.
+            Log.i(TAG, "SplashAdDisplayListener onAdClick.");
+        }
+    };
+
+    private void loadAd() {
+        Log.i(TAG, "Start to load ad");
+
+        AdParam adParam = new AdParam.Builder().build();
+
+        splashView = findViewById(R.id.splashAdView);
+        splashView.setAdDisplayListener(adDisplayListener);
+
+        // Set a logo image.
+        splashView.setLogoResId(R.mipmap.ic_launcher);
+        // Set logo description.
+        splashView.setMediaNameResId(R.string.app_name);
+        // Set the audio focus type for a video splash ad.
+        splashView.setAudioFocusType(AudioFocusType.NOT_GAIN_AUDIO_FOCUS_WHEN_MUTE);
+
+        splashView.load(getString(R.string.ad_splash), ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, adParam, splashAdLoadListener);
+        Log.i(TAG, "Finished loading ad");
+
+        // Remove the timeout message from the message queue.
+        timeoutHandler.removeMessages(MSG_AD_TIMEOUT);
+        // Send a delay message to ensure that the app home screen can be displayed when the ad display times out.
+        timeoutHandler.sendEmptyMessageDelayed(MSG_AD_TIMEOUT, AD_TIMEOUT);
+    }
+
+    /**
+     * Set this parameter to true when exiting the app to ensure that the app home screen is not displayed.
+     */
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "SplashActivity onStop.");
+        // Remove the timeout message from the message queue.
+        timeoutHandler.removeMessages(MSG_AD_TIMEOUT);
+        hasPaused = true;
+        super.onStop();
+    }
+
+    /**
+     * Call this method when returning to the splash ad screen from another screen to access the app home screen.
+     */
+    @Override
+    protected void onRestart() {
+        Log.i(TAG, "SplashActivity onRestart.");
+        super.onRestart();
+        hasPaused = false;
+        proceedToMainActivity();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "SplashActivity onDestroy.");
+        super.onDestroy();
+        if (splashView != null) {
+            splashView.destroyView();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "SplashActivity onPause.");
+        super.onPause();
+        if (splashView != null) {
+            splashView.pauseView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "SplashActivity onResume.");
+        super.onResume();
+        if (splashView != null) {
+            splashView.resumeView();
+        }
+    }
+
+    /**
+     * Switch from the splash ad screen to the app home screen when the ad display is complete.
+     */
+    private void proceedToMainActivity() {
+        if (!hasPaused) {
+            hasPaused = true;
+            Intent mainActivityIntent = new Intent(this, MainActivity.class);
+            startActivity(mainActivityIntent);
+
+            new Handler().postDelayed(this::finish, 1000);
+        }
+    }
+
 }
