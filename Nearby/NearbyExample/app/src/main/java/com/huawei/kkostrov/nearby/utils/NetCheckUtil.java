71f3013b2c7695590dc74b0617a8b3f00fa4da43
Kirill Kostrov <kostrov.kirill@huawei.com>
2020-10-13 12:17:26 +0300

    code cleanup

--- Nearby/NearbyExample/app/src/main/java/com/huawei/kkostrov/nearby/utils/NetCheckUtil.java
+++ Nearby/NearbyExample/app/src/main/java/com/huawei/kkostrov/nearby/utils/NetCheckUtil.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.huawei.kkostrov.nearby.utils;
 
 import android.content.Context;
@@ -21,23 +5,12 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
 
-/**
- * Network Check Util
- *
- * @since 2020-06-04
- */
 public class NetCheckUtil {
     private static final String TAG = "NetCheckUtil";
 
     private NetCheckUtil() {
     }
 
-    /**
-     * Check Is Network Available
-     *
-     * @param context Context
-     * @return true:Network is available
-     */
     public static boolean isNetworkAvailable(Context context) {
         boolean mobileConnection = isMobileConnection(context);
         boolean wifiConnection = isWifiConnection(context);
@@ -48,12 +21,6 @@ public class NetCheckUtil {
         return true;
     }
 
-    /**
-     * Is Mobile Connection
-     *
-     * @param context Context
-     * @return true:Mobile is connection
-     */
     public static boolean isMobileConnection(Context context) {
         Object object = context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (!(object instanceof ConnectivityManager)) {
@@ -68,12 +35,6 @@ public class NetCheckUtil {
         return false;
     }
 
-    /**
-     * Is WIFI Connection
-     *
-     * @param context Context
-     * @return true:wifi is connection
-     */
     public static boolean isWifiConnection(Context context) {
         Object object = context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (!(object instanceof ConnectivityManager)) {
