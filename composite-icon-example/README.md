e557a4f0ba2077390e8f30e1f2a57e537e3d176a
Kirill Kostrov <kostrov.kirill@huawei.com>
2021-05-20 16:15:11 +0300

    composite icon

---
+++ composite-icon-example/README.md
@@ -0,0 +1,10 @@
+# bumper-app-example
+
+To setup bumper app:
+
+1. Change bumper app package name to match you main app
+2. Put that package name in the code, where AppGallery page is called using intent
+3. Sign this bumper app using the same certificate, as your main app
+4. Make sure, versionCode of the bumper app is lower, that versionCode of your main app
+5. Style bumper app to suit your look & feel
+6. Make sure your bumper app signed and minified APK is less that 2Mb
---
+++ composite-icon-example/app/src/main/java/com/huawei/example/compositeicon/MainActivity.java
@@ -0,0 +1,34 @@
+package com.huawei.example.compositeicon;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void UpdateClick(View view) {
+        try {
+            // Здесь необходимо использовать имя пакета вашего приложения. Так как имя пакета и сертификат
+            // у бампера такой же, как у основного приложения, он просто будет заменён основным приложением при обновлении.
+            // versionCode в build.gradle должен быть заведомо ниже, чем у основного приложения
+
+            String appId = "your.app.package.name";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("appmarket://details?id=" + appId));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setPackage("com.huawei.appmarket.tv");
+            startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(this, "DEBUG: AppGallery not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
