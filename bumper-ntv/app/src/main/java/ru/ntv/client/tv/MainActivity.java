fdf9d104c77213f39b1dc5d8feb5895cc76bcf9e
Kirill Kostrov <kostrov.kirill@huawei.com>
2021-05-20 16:15:01 +0300

    updated ntv bumper

--- bumper-ntv/app/src/main/java/ru/ntv/client/tv/MainActivity.java
+++ bumper-ntv/app/src/main/java/ru/ntv/client/tv/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startGooglePlay();
+        finish();
     }
 
     private void startGooglePlay() {
@@ -18,6 +20,7 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         try {
             intent.setData(Uri.parse("appmarket://details?id=" + packageName));
+            intent.setPackage("com.huawei.appmarket.tv");
             startActivity(intent);
         } catch (Throwable ignored) {
             finish();
