c36192d2519c8730ad00131baa1bc04abc88e6ad
Kirill Kostrov <kostrov.kirill@huawei.com>
2021-10-20 11:00:26 +0300

    updated references

--- hmsstudy/app/src/main/java/com/huawei/kkstudy/LabActivities/MapActivity.java
+++ hmsstudy/app/src/main/java/com/huawei/kkstudy/LabActivities/MapActivity.java
@@ -3,12 +3,17 @@ package com.huawei.kkstudy.LabActivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.huawei.agconnect.config.AGConnectServicesConfig;
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.MapView;
 import com.huawei.hms.maps.MapsInitializer;
 import com.huawei.hms.maps.OnMapReadyCallback;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.Marker;
+import com.huawei.hms.maps.model.MarkerOptions;
 import com.huawei.kkstudy.R;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -18,6 +23,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private HuaweiMap hMap;
 
     private MapView mMapView;
+
+    private Marker mMarker;
 
     private String apiKey;
 
@@ -44,7 +51,32 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(HuaweiMap huaweiMap) {
+
         hMap = huaweiMap;
+        initMarkerClickListener();
+        addMarker();
+
+    }
+
+    public void initMarkerClickListener() {
+        hMap.setOnMarkerClickListener(new HuaweiMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), "onMarkerClick:" + marker.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    public void addMarker() {
+        if (null != mMarker) {
+            mMarker.remove();
+        }
+        MarkerOptions options = new MarkerOptions().position(new LatLng(48.893478, 2.334595));
+        options.title("Hello Huawei Map");
+        options.snippet("This is a snippet!");
+        mMarker = hMap.addMarker(options);
+
     }
 
     @Override
